8fe5d366280cb9871dbfea04dab123318fe241c4
Minhazul-Islam18 <minhaz019166@gmail.com>
2024-01-03 18:38:50 +0600

    Spin api updated

--- src/Http/Controllers/Api/MemberSpinHandlerController.php
+++ src/Http/Controllers/Api/MemberSpinHandlerController.php
@@ -3,11 +3,12 @@
 namespace Luminouslabs\Installer\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
-use App\Models\Member;
+// use App\Models\Member;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Validator;
+use Luminouslabs\Installer\Models\Member;
 
 class MemberSpinHandlerController extends Controller
 {
@@ -30,46 +31,52 @@ class MemberSpinHandlerController extends Controller
         // Attempt to authenticate the user using the 'member_api' guard
         $user = Auth::guard('member_api')->user();
 
-        DB::beginTransaction();
+        if ($user) {
+            DB::beginTransaction();
+            try {
+                // Attach campaign to member with specified data
+                $member = Member::with(['campaigns'])->findOrFail($user->id);
+                $e = $member->campaigns()->attach($request['campaign_id'], [
+                    'spinner_round' => $request['spinner_round'],
+                    'rewards' => json_encode($request['rewards']),
+                ]);
 
-        try {
-            // Attach campaign to member with specified data
-            $member = member(['campaigns']);
-            $e = $member->campaigns()->attach($request['campaign_id'], [
-                'spinner_round' => $request['spinner_round'],
-                'rewards' => json_encode($request['rewards']),
-            ]);
-
-            // Update spinner counts
-            $spin = DB::table('member_spinner_count')
-                ->where('member_id', Auth::id())
-                ->where('campaign_id', $request['campaign_id'])
-                ->first();
-            if ($spin) {
-                DB::table('member_spinner_count')
+                // Update spinner counts
+                $spin = DB::table('member_spinner_count')
                     ->where('member_id', Auth::id())
                     ->where('campaign_id', $request['campaign_id'])
-                    ->update([
-                        'remaining_spin' => $spin->remaining_spin - 1,
-                    ]);
+                    ->first();
+                if ($spin) {
+                    DB::table('member_spinner_count')
+                        ->where('member_id', Auth::id())
+                        ->where('campaign_id', $request['campaign_id'])
+                        ->update([
+                            'remaining_spin' => $spin->remaining_spin - 1,
+                        ]);
+                }
+
+                DB::commit();
+
+                return response()->json([
+                    "message" => "Spin round data successfully processed.",
+                    "status" => 200,
+                ], 200);
+
+            } catch (\Exception $e) {
+                dd($e);
+                // Something went wrong, rollback the transaction
+                DB::rollback();
+
+                // Handle the exception as needed
+                return response()->json([
+                    "error" => "An error occurred while processing the spin round data.",
+                    "status" => 202,
+                ], 202);
             }
 
-            DB::commit();
-
-            return response()->json([
-                "message" => "Spin round data successfully processed.",
-                "status" => 200,
-            ], 200);
-
-        } catch (\Exception $e) {
-            // Something went wrong, rollback the transaction
-            DB::rollback();
-
-            // Handle the exception as needed
-            return response()->json([
-                "error" => "An error occurred while processing the spin round data.",
-                "status" => 202,
-            ], 202);
+        } else {
+            // Token is not valid or user is not authenticated
+            return response()->json(['message' => 'Token verification failed'], 401);
         }
 
     }
