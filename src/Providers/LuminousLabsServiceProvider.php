39298a1e7fdeed2a3d5879f9040fae303435afae
Minhazul-Islam18 <minhaz019166@gmail.com>
2024-01-03 11:16:36 +0600

    Code Updated

--- src/Providers/LuminousLabsServiceProvider.php
+++ src/Providers/LuminousLabsServiceProvider.php
@@ -20,10 +20,6 @@ class LuminousLabsServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        $this->publishes([
-            __DIR__ . '/../Helpers' => app_path('Helpers'),
-        ], 'helpers');
-
         $this->loadRoutesFrom(__DIR__ . '/../Routes/web.php');
         $this->loadRoutesFrom(__DIR__ . '/../Routes/api.php');
         Route::prefix('api')
